9303f148642052b3180878915983a3fa25b33263
Andrew Schwenk <schwenk.andrew@gmail.com>
2020-07-31 19:21:33 -0500

    Adding files

---
+++ src/flask_app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+from src.eightball import get_answer
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+	prompt = 'Ask the Magic Eightball a Yes/No question'
+	return render_template('index.html', prompt=prompt)
+
+
+@app.route('/eightball')
+def eightball():
+	answer = get_answer()
+	question = request.args.get('question')
+	return render_template('eightball.html', question=question, answer=answer)
+
+
+if __name__ == '__main__':
+    app.run()
